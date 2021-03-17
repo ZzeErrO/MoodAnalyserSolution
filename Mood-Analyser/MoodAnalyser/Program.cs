bc9e796d2b16d2654997ce8b21c1c83f4d2e6070
prashikjaware <prashikjaware123@gmail.com>
2021-03-17 17:51:49 +0530

    [Prashik]Added: Analyse Mood using Exception and Reflection in C#

---
+++ Mood-Analyser/MoodAnalyser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodAnalyser mood = new MoodAnalyser("sad");
+            mood.analyseMoodLive1();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Mood-Analyser/MoodAnalyserMSTestWithCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnalyserMSTestWithCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodAnalyserMSTestWithCore")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d752f224-40b7-4af5-96fd-32bcc8d44ca0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
